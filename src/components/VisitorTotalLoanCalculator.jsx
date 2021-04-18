9c30711429b71e9f1be55642d78adee67abd3de7
Alex_Bellamy <alexlbellamy@gmail.com>
2021-04-18 15:58:21 +0200

    setting initial loan detail values, a handleloanchange function which updates this state accordingly and an addadditionalloans function with corresponding save button and map function which renders a further set of input fields, together with an initial table structure

--- src/components/VisitorTotalLoanCalculator.jsx
+++ src/components/VisitorTotalLoanCalculator.jsx
@@ -1,12 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialValues = [
+  {
+    name: "",
+    loanAmount: "",
+    monthlyFees: "",
+    apr: "",
+  },
+];
 
 const VisitorTotalLoanCalculator = () => {
+  const [initial, setInitial] = useState(initialValues);
+
+  const handleLoanChanges = (e) => {
+    const updatedValues = [...initial];
+    updatedValues[e.target.dataset.id][e.target.name] = e.target.value;
+    setInitial(updatedValues);
+  };
+
+  const addAdditionalLoan = () => {
+    setInitial((prevLoans) => [
+      ...prevLoans,
+      { name: "", loanAmount: "", monthlyFees: "", apr: "" },
+    ]);
+  };
+
   return (
-    <div>
-      <input name="name" placeholder="Creditor name" type="text" />
-      <input name="loanAmount" placeholder="Total loan amount" type="number" />
-      <input name="monthlyFees" placeholder="Monthly fees" type="number" />
-      <input name="apr" placeholder="APR" type="number" />
+    <div class="table">
+      <div class="tableContent">
+        <div class="tableHeader">
+          <div class="tableRow">
+            <div class="tableBody">
+              {initial.map((loan, index) => (
+                <div class="tableRow" key={index}>
+                  <div class="tableData">
+                    <input
+                      name="name"
+                      placeholder="Creditor name"
+                      data-id={index}
+                      data-cy="creditorName"
+                      type="text"
+                      value={loan.name}
+                      onChange={handleLoanChanges}
+                    />
+                  </div>
+                  <div className="tableData">
+                    <input
+                      name="loanAmount"
+                      placeholder="Total loan amount"
+                      data-id={index}
+                      data-cy="loanAmount"
+                      type="number"
+                      value={loan.loanAmount}
+                      onChange={handleLoanChanges}
+                    />
+                  </div>
+                  <div class="tableData">
+                    <input
+                      name="monthlyFees"
+                      placeholder="Monthly fees"
+                      data-id={index}
+                      data-cy="monthlyFees"
+                      type="number"
+                      value={loan.monthlyFees}
+                      onChange={handleLoanChanges}
+                    />
+                  </div>
+                  <div class="tableData">
+                    <input
+                      name="apr"
+                      placeholder="APR"
+                      data-id={index}
+                      data-cy="apr"
+                      type="number"
+                      value={loan.apr}
+                      onChange={handleLoanChanges}
+                    />
+                  </div>
+                </div>
+              ))}
+              <div class="tableRow">
+                <div class="tableData">
+                  <button
+                    onClick={() => {
+                      addAdditionalLoan();
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
