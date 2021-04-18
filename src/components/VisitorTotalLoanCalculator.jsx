f3e15f0c500b298471df058ea5fae256c7c3ff52
Alex_Bellamy <alexlbellamy@gmail.com>
2021-04-18 17:26:12 +0200

    adding a title to the component and styling the component, title, table, input fields and save button

--- src/components/VisitorTotalLoanCalculator.jsx
+++ src/components/VisitorTotalLoanCalculator.jsx
@@ -52,94 +52,110 @@ const VisitorTotalLoanCalculator = () => {
   };
 
   return (
-    <div class="table">
-      <div class="tableContent">
-        <div class="tableHeader">
-          <div class="tableRow">
-            <div class="tableData">
-              <div>Creditor name</div>
-            </div>
-            <div class="tableData">
-              <div>Loan amount (sek)</div>
-            </div>
-            <div class="tableData">
-              <div>Montly fees (sek)</div>
-            </div>
-            <div class="tableData">
-              <div>APR (%)</div>
-            </div>
-          </div>
-          <div class="tableBody">
-            {initial.map((loan, index) => (
-              <div class="tableRow" key={index}>
-                <div class="tableData">
-                  <input
-                    name="name"
-                    placeholder="Creditor name"
-                    data-id={index}
-                    data-cy="creditorName"
-                    type="text"
-                    value={loan.name}
-                    onChange={handleLoanChanges}
-                  />
-                </div>
-                <div className="tableData">
-                  <input
-                    name="loanAmount"
-                    placeholder="Total loan amount"
-                    data-id={index}
-                    data-cy="loanAmount"
-                    type="number"
-                    value={loan.loanAmount}
-                    onChange={handleLoanChanges}
-                  />
-                </div>
-                <div class="tableData">
-                  <input
-                    name="monthlyFees"
-                    placeholder="Monthly fees"
-                    data-id={index}
-                    data-cy="monthlyFees"
-                    type="number"
-                    value={loan.monthlyFees}
-                    onChange={handleLoanChanges}
-                  />
-                </div>
-                <div class="tableData">
-                  <input
-                    name="apr"
-                    placeholder="APR"
-                    data-id={index}
-                    data-cy="apr"
-                    type="number"
-                    value={loan.apr}
-                    onChange={handleLoanChanges}
-                  />
-                </div>
-              </div>
-            ))}
-            <div class="tableRow">
-              <div class="tableData">
-                <button
-                  onClick={() => {
-                    addAdditionalLoan();
-                    getTotalLoanAmount();
-                    getTotalFees();
-                    getTotalApr();
-                  }}
-                >
-                  Save
-                </button>
-              </div>
-            </div>
-            <div class="tableFooter">
+    <div id="page">
+      <div id="title">
+        Like the Lindbergs, calculate your total loan position
+      </div>
+      <div id="photo">
+        <div class="table">
+          <div class="tableContent">
+            <div class="tableHeader">
               <div class="tableRow">
                 <div class="tableData">
-                  <div>Totals:</div>
+                  <div>Creditor name</div>
                 </div>
-                <div class="tableData">{totalLoanAmount}</div>
-                <div class="tableData">{totalMonthlyFees}</div>
-                <div class="tableData">{totalApr}</div>
+                <div class="tableData">
+                  <div>Loan amount (sek)</div>
+                </div>
+                <div class="tableData">
+                  <div>Monthly fees (sek)</div>
+                </div>
+                <div class="tableData">
+                  <div>APR (%)</div>
+                </div>
+              </div>
+              <div class="tableBody">
+                {initial.map((loan, index) => (
+                  <div class="tableRow" key={index}>
+                    <div class="tableData">
+                      <input
+                      div
+                      class="inputField"
+                        name="name"
+                        placeholder="Creditor name"
+                        data-id={index}
+                        data-cy="creditorName"
+                        type="text"
+                        value={loan.name}
+                        onChange={handleLoanChanges}
+                      />
+                    </div>
+                    <div className="tableData">
+                      <input
+                      div
+                      class="inputField"
+                        name="loanAmount"
+                        placeholder="Total loan amount"
+                        data-id={index}
+                        data-cy="loanAmount"
+                        type="number"
+                        value={loan.loanAmount}
+                        onChange={handleLoanChanges}
+                      />
+                    </div>
+                    <div class="tableData">
+                      <input
+                      div
+                      class="inputField"
+                        name="monthlyFees"
+                        placeholder="Monthly fees"
+                        data-id={index}
+                        data-cy="monthlyFees"
+                        type="number"
+                        value={loan.monthlyFees}
+                        onChange={handleLoanChanges}
+                      />
+                    </div>
+                    <div class="tableData">
+                      <input
+                      div
+                      class="inputField"
+                        name="apr"
+                        placeholder="APR"
+                        data-id={index}
+                        data-cy="apr"
+                        type="number"
+                        value={loan.apr}
+                        onChange={handleLoanChanges}
+                      />
+                    </div>
+                  </div>
+                ))}
+                <div class="tableRow">
+                  <div class="tableData">
+                    <button
+                    id="saveButton"
+                      onClick={() => {
+                        addAdditionalLoan();
+                        getTotalLoanAmount();
+                        getTotalFees();
+                        getTotalApr();
+                      }}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+                <div class="tableFooter">
+                  <div class="tableRow">
+                    <div class="tableData">
+                      <div>Totals:</div>
+                    </div>
+                    <div class="tableData">{totalLoanAmount}</div>
+                    <div class="tableData">{totalMonthlyFees}</div>
+                    <div class="tableData">{totalApr}</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
