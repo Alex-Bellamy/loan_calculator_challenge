e35d078c19e4694cbf7402e5e98297158d026695
Alex_Bellamy <alexlbellamy@gmail.com>
2021-04-18 13:21:09 +0200

    writing initial specs visitor can calculate their total liability

---
+++ cypress/integration/vistorCanCalculateTheirTotalLoanLiability.feature.js
@@ -0,0 +1,17 @@
+describe("Visitor can calculate their total loan liability", () => {
+  it("successfully", () => {
+    cy.visit("/");
+    cy.get("[data-cy=loanForm]").within(() => {
+      cy.get("[data-cy=creditorName]").type("ICA");
+      cy.get("[data-cy=loanAmount]").type("36000");
+      cy.get("[data-cy=monthlyFees]").type("50");
+      cy.get("[data-cy=apr]").type("7.54");
+      cy.get("[data-cy=saveButton]").click();
+    });
+    cy.get("[data-cy=loanTotals]").within(() => {
+      cy.get("[data-cy=totalLoanAmount]").should("contain", "36000");
+      cy.get("[data-cy=totalMonthlyFees]").should("contain", "50");
+      cy.get("[data-cy=totalApr]").type("7.54");
+    })
+  });
+});
